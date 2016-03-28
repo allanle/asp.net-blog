d6476f3c56ff2ee5f13f405b6a41617f2a8b2658
allanle <ale11@my.bcit.ca>
2016-03-27 23:32:14 -0700

    Added database schema

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,8 +10,8 @@ namespace asp.net_blog.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/admin/styles").Include("~/content/styles/bootstrap/css")
-                .Include("~/content/styles/admin.css")
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap/css")
                 .Include("~/content/styles/admin.css"));
 
             bundles.Add(new StyleBundle("~/styles")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # asp.net-blog
 My ASP.NET blogging project
+
+.\packages\FluentMigrator.1.6.1\tools\Migrate.exe --db=mysql --target=bin\asp.net-blog.dll --configPath=Web.config -c=MainDb -t=rollback
