33e141c7f894928f9ec04f443029482ae2b6bdf7
allanle <ale11@my.bcit.ca>
2016-04-11 15:05:31 -0700

    Added frontend Posts controller Implemented all markup for views

---
+++ ViewModels/Posts.cs
@@ -0,0 +1,21 @@
+﻿using asp.net_blog.Infrastructure;
+using asp.net_blog.Models;
+
+namespace asp.net_blog.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsShow
+    {
+        public Post Post { get; set; }
+    }
+
+    public class PostsTag
+    {
+        public Tag Tag { get; set; }
+        public PagedData<Post> Posts { get; set; }
+    }
+}
